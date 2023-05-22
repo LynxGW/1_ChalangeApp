3fa04cf25d887b807dd1a9e9687aeaf57d04b6be
LynxGW <gwisniew@wp.pl>
2023-05-22 20:44:33 +0200

    Home work day 4

--- 1_ChalangeApp/1_ChalangeApp/Program.cs
+++ 1_ChalangeApp/1_ChalangeApp/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+string myName = "Ewa";
+bool isMen = false;
+int myAge = 33;
+
+if (isMen == false)
+{
+    if (myName == "Ewa" && myAge == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (myAge < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else Console.WriteLine("Kobieta powyżej 30 lat");
+
+}
+else if (myAge < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else Console.WriteLine("Pełnoletni mężczyzna");
